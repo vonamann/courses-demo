3b3ed1a84fe5ba26a58e1e2cddc1267174ebd03f
Johann Aman <johann.aman@gmail.com>
2021-03-12 02:01:02 +0300

    Validate lesson limit and duration

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -1,3 +1,5 @@
 class PromoCode < ApplicationRecord
   belongs_to :course
+  validates :lesson_limit, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :duration,     numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 25 }
 end
